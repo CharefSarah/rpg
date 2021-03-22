06037255a8206d57d40602aee4ae84bffa24e36d
WilliamTouboul <will.touboul@gmail.com>
2021-03-22 18:32:22 +0100

    stat perso

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Consctructeur objet Hero.
+function Hero(health, mana, defence, attack, weakness, resistance) {
+    this.health = health;
+    this.mana = mana;
+    this.defence = defence;
+    this.attack = attack;
+    this.weakness = weakness;
+    this.resistance = resistance;
+  }
+
+  var knight  = new Hero(500,0,30,50,thunder,sword);
+
+  var mage = new Hero(300,150,10,75,sword,magic);
+
+  var archer = new Hero(400,50,20,75,none,none);
+
+  //Constructeur objet Méchant.
+function BadGuy(health,defence,attack,weakness,resistance) {
+    this.health = health;
+    this.defence = defence;
+    this.attack = attack;
+    this.weakness = weakness;
+    this.resistance = resistance;
+}
+
+var orc = new BadGuy(300,10,25,all,none);
+
+var darkKnight = new BadGuy(1000,30,40,none,sword);
