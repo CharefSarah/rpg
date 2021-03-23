55abe0c7f575f7f6203c0e0241b8ee7a6aee15ed
WilliamTouboul <will.touboul@gmail.com>
2021-03-23 15:34:11 +0100

    affichage de trucs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,145 @@
 // Consctructeur objet Hero.
 function Hero(health, defence, attack, weakness, resistance) {
-    this.health = health;
-    this.defence = defence;
-    this.attack = attack;
-    this.weakness = weakness;
-    this.resistance = resistance;
-  }
-
-  var knight  = new Hero(500,30,50,thunder,sword);
-
-  var mage = new Hero(300,10,75,sword,magic);
-
-  var archer = new Hero(400,20,75,none,none);
-
-  //Constructeur objet Méchant.
-function BadGuy(health,defence,attack,weakness,resistance) {
-    this.health = health;
-    this.defence = defence;
-    this.attack = attack;
-    this.weakness = weakness;
-    this.resistance = resistance;
+  this.health = health;
+  this.defence = defence;
+  this.attack = attack;
+  this.weakness = weakness;
+  this.resistance = resistance;
 }
 
-var orc = new BadGuy(300,10,25,all,none);
+// Creation Classe Hero
+var knight = new Hero(500, 30, 30, "thunder", "sword");
 
-var darkKnight = new BadGuy(1000,30,40,none,sword); 
+var mage = new Hero(300, 10, 40, "sword", "magic");
+
+var archer = new Hero(400, 20, 35, "none", "none");
+
+// Test affichage
+window.addEventListener("load",function() {
+  // Affichage Stat Knight 
+  document.getElementById("knightHP").innerHTML = knight.health;
+  document.getElementById("knightDefence").innerHTML = knight.defence;
+  document.getElementById("knightAttack").innerHTML = knight.attack;
+  document.getElementById("knightWeakness").innerHTML = knight.weakness;
+  document.getElementById("knightResistance").innerHTML = knight.resistance;
+  // Affichage Stat Mage 
+  document.getElementById("mageHP").innerHTML = mage.health;
+  document.getElementById("mageDefence").innerHTML = mage.defence;
+  document.getElementById("mageAttack").innerHTML = mage.attack;
+  document.getElementById("mageWeakness").innerHTML = mage.weakness;
+  document.getElementById("mageResistance").innerHTML = mage.resistance;
+  // Affichage Stat Archer
+  document.getElementById("archerHP").innerHTML = archer.health;
+  document.getElementById("archerDefence").innerHTML = archer.defence;
+  document.getElementById("archerAttack").innerHTML = archer.attack;
+  document.getElementById("archerWeakness").innerHTML = archer.weakness;
+  document.getElementById("archerResistance").innerHTML = archer.resistance;
+});
+
+//Constructeur objet Méchant.
+function BadGuy(name, health, defence, attack, weakness, resistance) {
+  this.name = name;
+  this.health = health;
+  this.defence = defence;
+  this.attack = attack;
+  this.weakness = weakness;
+  this.resistance = resistance;
+}
+
+// compteur de round
+round = 0;
+
+//Creation du méchant : ( non vraiment, meilleure commentaire ever )
+function CreateBadGuy() {
+  if (round == 10) {
+    var badGuy = new BadGuy("Dark Knight", 1000, 30, 40, "none", "sword");
+  } else {
+    var badGuy = new BadGuy("Orc", 300, 10, 25, "all", "none");
+  }
+  return badGuy
+}
+
+window.addEventListener("load",function() {
+  badGuy =  CreateBadGuy();
+  document.getElementById("badGuyName").innerHTML = badGuy.name;
+  document.getElementById("badGuyHealth").innerHTML = badGuy.health;
+  document.getElementById("badGuyDefence").innerHTML = badGuy.defence;
+  document.getElementById("badGuyAttack").innerHTML = badGuy.attack;
+  document.getElementById("badGuyWeakness").innerHTML = badGuy.weakness;
+  document.getElementById("badGuyResistance").innerHTML = badGuy.resistance;
+
+});
 
 // Fonction Coup critique
 function Crit() {
   // Renvoi un nombre entre 1 et 100
   crit = Math.floor(Math.random() * 100) + 1;
-  if (crit<=5 ) {
+  if (crit <= 5) {
     return TRUE;
   }
 }
 
-// petit steak
+/////////////////////////////////////// KNIGHT 
+// Petit steak
 function KnightBaseAttack() {
-  
+
   attackDamage = knight.attack;
-  if(crit()) {
-    attackDamage = attackDamage *2;
+  if (crit()) {
+    attackDamage = attackDamage * 2;
   }
 
   return attackDamage;
 }
 
-// gros steak
+// Gros steak
 function KnightHeavyAttack() {
 
   attackDamage = knight.attack * 2;
-  if(crit()) {
-    attackDamage = attackDamage *2;
+  if (crit()) {
+    attackDamage = attackDamage * 2;
   }
 
- return attackDamage;
+  return attackDamage;
 
 }
+
+/////////////////////////////////////// Mage
+// Petit Steak 
+function MageBaseAttack() {
+  attackDamage = mage.attack;
+
+  if (crit()) {
+    attackDamage = attackDamage * 2;
+  }
+
+  return attackDamage;
+}
+
+//Gros Steak
+function MageHeavyAttack() {
+  attackDamage = mage.attack * 2;
+  if (crit()) {
+    attackDamage = attackDamage * 2;
+  }
+
+  return attackDamage;
+}
+
+////////////////////////////////////// Archer
+// Petit steak
+function ArcherBaseAttack() {
+  attackDamage = archer.attack;
+
+  if (crit()) {
+    attackDamage = attackDamage * 2;
+  }
+}
+
+// Gros Steak
+function ArcherHeavyAttack() {
+  attackDamage = archer.attack * 2;
+  if (crit()) {
+    attackDamage = attackDamage * 2;
+  }
+}
+
