2917119a8bea0def50d4ce19cab9cbce63623329
WilliamTouboul <will.touboul@gmail.com>
2021-03-27 15:18:37 +0100

    debug round

--- assets/js/script.js
+++ assets/js/script.js
@@ -281,7 +281,7 @@ function BadGuy(name, health, maxHealth, defence, attack, weakness, resistance) 
 }
 
 // Compteur de round
-var round = 31;
+var round = 1;
 document.getElementById("round").innerHTML = round;
 
 //Creation du méchant selon le nombre de round: 
